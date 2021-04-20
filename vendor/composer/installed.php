f3cb23cc3e616bf801ba73b007593182170b643e
yu.xiong <qq11!!>
2021-04-20 14:28:24 +0800

    test

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'xy/timi-cdn-hy',
+  ),
+  'versions' => 
+  array (
+    'xy/timi-cdn-hy' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
